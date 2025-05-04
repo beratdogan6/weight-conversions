bf2da6ec22b638cdba3de6153db91e0b0b5c3fe5
beratdogan6 <102744824+beratdogan6@users.noreply.github.com>
2025-05-04 21:54:24 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,4 @@
 
 namespace Beratdogan6\WeightConversions\Tests;
 
-abstract class TestCase extends \PHPUnit\Framework\TestCase
-{
-
-}
+abstract class TestCase extends \PHPUnit\Framework\TestCase {}
